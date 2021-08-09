346a3cda277351d1e0917d3ff3aa6d7e4ae8e786
ctrlfreak453 <74403567+ctrlfreak453@users.noreply.github.com>
2021-08-09 18:24:37 +0200

    meshrenderer

---
+++ Assets/Scripts/NoiseMapGeneration.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoiseMapGeneration : MonoBehaviour
+{
+    public float[,] GenerateNoiseMap (int mapWidth, int mapDepth, float scale)
+    {
+        float[,] noiseMap = new float[mapWidth+1, mapDepth+1];
+        for(int x = 0; x <= mapWidth; x++)
+        {
+            for( int y = 0;y <= mapDepth; y++)
+            {
+                float sampleX = x / scale;
+                float sampleY = y / scale;
+
+                float noise = Mathf.PerlinNoise(sampleX, sampleY);
+                noiseMap[x, y] = noise;
+            }
+        }
+        return noiseMap;
+    }
+
+}
